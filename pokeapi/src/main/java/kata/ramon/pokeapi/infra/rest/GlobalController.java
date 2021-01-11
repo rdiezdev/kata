02ce987fd61fc3217cd1ca8de80ca79ca5a93ed4
Ramon E. D <rdiezdev@gmail.com>
2021-01-11 18:07:55 +0100

    fixed / improved error handling

--- pokeapi/src/main/java/kata/ramon/pokeapi/infra/rest/GlobalController.java
+++ pokeapi/src/main/java/kata/ramon/pokeapi/infra/rest/GlobalController.java
@@ -1,5 +1,6 @@
 package kata.ramon.pokeapi.infra.rest;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -7,12 +8,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.validation.ConstraintViolationException;
 import java.time.LocalDateTime;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalController {
@@ -21,10 +24,28 @@ public class GlobalController {
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     protected RestError handleException(ConstraintViolationException exception) {
+
+        Map<String, Collection<String>> errors = extractErrors(exception);
+
         return  RestError.builder()
                 .error("VALIDATION_ERROR")
-                .message(exception.getMessage())
+                .errors(errors)
                 .timestamp(LocalDateTime.now()).build();
+    }
+
+    private Map<String, Collection<String>> extractErrors(ConstraintViolationException ex) {
+        Map<String, Collection<String>> errors = new LinkedHashMap<>();
+        ex.getConstraintViolations().forEach(constraintViolation -> {
+            String queryParamPath = constraintViolation.getPropertyPath().toString();
+            String queryParam = queryParamPath.contains(".") ?
+                    queryParamPath.substring(queryParamPath.indexOf(".") + 1) :
+                    queryParamPath;
+            String errorMessage = constraintViolation.getMessage();
+            Collection<String> perQueryParamErrors = errors.getOrDefault(queryParam, new ArrayList<>());
+            perQueryParamErrors.add(errorMessage);
+            errors.put(queryParam, perQueryParamErrors);
+        });
+        return errors;
     }
 
     @ExceptionHandler(Exception.class)
@@ -48,9 +69,11 @@ public class GlobalController {
     @Builder
     @RequiredArgsConstructor
     @Getter
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class RestError {
         private final String error;
         private final String message;
+        private final Map<String, Collection<String>> errors;
         private final LocalDateTime timestamp;
     }
 }
